39526ec605d6723ce4b02d892a9e1fc0ee3ee733
Simina Rosioru <simina.rosioru@gmail.com>
2025-11-02 04:51:46 +0200

    Create MountainsRomania.java
    create the MountainsRomania class with the JFrame window

---
+++ MountainsRomania.java
@@ -0,0 +1,13 @@
+package mainGraphicalInterface;
+
+import javax.swing.JFrame;
+
+public class MountainsRomania {
+
+	public static void main(String[] args) {
+		//create JFrame window
+		JFrame window = new JFrame("first graphical interface");
+		window.setVisible(true);
+		window.setSize(500, 800);
+		window.setLocationRelativeTo(null);
+		

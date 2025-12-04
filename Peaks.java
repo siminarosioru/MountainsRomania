512c025e6e9d6c4a9059cfc8d0676324bbd76125
Simina Rosioru <simina.rosioru@gmail.com>
2025-12-04 07:12:23 +0200

    Update Peaks.java
    invoked when an action occurs

--- Peaks.java
+++ Peaks.java
@@ -54,7 +54,9 @@ public class Peaks {
 
 		//add an ActionListener that will receive an ActionEvent when a selection has been made
 		myComboBox.addActionListener(new ActionListener() {
-
+			public void actionPerformed(ActionEvent e) {
+				
+			}
 		});
 						
 		
